c67b804fda18c574352da3c5720f2aa2871dc972
Zachary Sluder <zachary.sluder@gmail.com>
2018-05-22 21:47:12 -0700

    loading features

--- addr-finder.cpp
+++ addr-finder.cpp
@@ -7,15 +7,18 @@
 
 using namespace std;
 
+// Features loaded in from config
+map<string, vector<string>> features;
+
 /**
  * Trim whitespace on string
  */
 string trim(string& str)
 {
-    size_t startpos = str.find_first_not_of(" ");
-    size_t endpos = str.find_last_not_of(" ");
-	
-	return str.substr(startpos, endpos - startpos + 1);
+    size_t start = str.find_first_not_of(" ");
+    size_t end = str.find_last_not_of(" ");
+
+	return str.substr(start, end - start + 1);
 }
 
 /**
@@ -25,14 +28,15 @@ string createGram(string& instruction)
 {
 	string opcode = instruction.substr(0, instruction.find(" "));
 	string opperands = instruction.substr(opcode.size(), instruction.size());
-		
+
 	stringstream stream(opperands);
 	string opperand;
-	
+
+	// Loop through all instruction opperands
 	while (stream.good()) {
 		getline(stream, opperand, ',');
 		opperand = trim(opperand);
-		
+
 		if (opperand.substr(0, 2) == "#0") {
 			opperand = "num";
 		} else if (opperand.substr(0, 2) == "0x") {
@@ -40,31 +44,63 @@ string createGram(string& instruction)
 		} else {
 			opperand = "reg";
 		}
-		
+
 		opcode += "." + opperand;
 	}
-	
+
 	return opcode;
 }
 
 /**
- * Start
+ * Loads all the features we want to find
+ */
+void loadFeatures()
+{
+	ifstream file("features.txt");
+	string file_line, sensor, gram;
+
+	// Loop though file
+	while (getline(file, file_line)) {
+		istringstream stream(file_line);
+		
+		getline(stream, sensor, ' ');
+		
+		// Loop to load grams
+		while (!stream.eof()) {
+			getline(stream, gram, ' ');
+			
+			features[sensor].push_back(gram);
+		}
+	}
+
+	// Output features
+	cout << "===== LOADED FEATURES =====" << endl;
+	for (auto i = features.begin(); i != features.end(); i++) {
+		cout << i->first << endl;
+		
+		for (auto j = i->second.begin(); j != i->second.end(); j++) {
+			cout << "\t" << *j << endl;
+		}
+	}
+}
+
+/**
+ * Start of program
  */
 int main()
 {
-	// Open sample files
-	ifstream sample;
-	sample.open("sample.txt");
-
-	// Loop through instructions
-	string file_line, opcode, opperands;
+	loadFeatures();
 	
+	ifstream sample("sample.txt");
+
+	string file_line;
+
 	while (getline(sample, file_line)) {
 		file_line = createGram(file_line);
 	}
-	
+
 	// Cleanup
 	sample.close();
-	
+
 	return 0;
 }
