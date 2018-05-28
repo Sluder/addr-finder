c774d3d652afedbb4ab4babe8c6693019a25a7eb
Ken Schimpf <kswissthefourth@rs231225.resnet.wright.edu>
2018-05-28 19:45:53 -0400

    inital changes to create instruction object

--- addr-finder.cpp
+++ addr-finder.cpp
@@ -24,42 +24,86 @@ string trim(string& str)
 /**
  * Replaces opperands with normalized code
  */
-string createGram(string& instruction)
-{
-	string opcode = instruction.substr(0, instruction.find(" "));
-	string opperands = instruction.substr(opcode.size(), instruction.size());
+ class instructionObj{
+   //class type
+   public:
 
-	// Check instructions with no opperands
-	if (opperands == "") {
-		return opcode;
-	}
-	
-	stringstream stream(opperands);
-	string opperand;
+   //instance variables
+   std::vector<string> variables; //all variables in instruction line (ex: var1, var27, etc..)
+   std::vector<string> values; //all values in instruction line (ex: 0x102a, #0x80, al)
+   string objOpCode; //this has strings in it (ex: lda.num.mem)
+   string objGram;  //this has variables in it (ex: bcc.Var1.Var27)
+   int objNumber; //distinguishes each object from one another
 
-	// Loop through all instruction opperands
-	while (stream.good()) {
-		getline(stream, opperand, ',');
-		opperand = trim(opperand);
+ //turn this into the object's constructor potentially
+  instructionObj(string& instruction, int& object_number){
+    //set object's objNumber
+    objNumber = object_number;
 
-		if (opperand.substr(0, 2) == "#0") {
-			opperand = "num";
-		} else if (opperand.substr(0, 2) == "0x") {
-			opperand = "mem";
-		} else {
-			opperand = "reg";
-		}
+    //start filling object instances with file line data
+    string opcode = instruction.substr(0, instruction.find(" "));
 
-		opcode += "." + opperand;
-	}
+  	string opperands = instruction.substr(opcode.size(), instruction.size());
 
-	return opcode;
+  	// Check instructions with no opperands
+  	if (opperands == "") {
+  		return opcode;
+  	}
+
+  	stringstream stream(opperands);
+  	string opperand;
+
+  	// Loop through all instruction opperands
+  	while (stream.good()) {
+  		getline(stream, opperand, ',');
+  		opperand = trim(opperand);
+
+      //add operand to values array for current object (Needs updating)
+      values.push_back(opperand);
+
+      //USED FOR CREATING objGram
+      //loop through usedValues vector to see if the opperand has been used
+      int position = find(usedValues.begin(), usedValues.end(), opperand) - usedValues.begin())
+      if (position < usedValues.size()){
+        //variable has already been used
+        //create a variable based on that index and add to current object's variables array
+        string variable = "Var" + to_string(position);
+        variables.push_back(variable);
+      }else{
+        //add opperand to end of the usedValues vector
+        usedValues.push_back(opperand);
+        //get index where operand was just placed in array and create variable based on it
+        string variable = "Var" + to_string(usedValues.size() - 1);
+        //add new variable to instruction object's variable vector (needs updating)
+        variables.push_back(variable);
+
+      }
+
+      //USED FOR CONSTRUCTING objOpCode
+  		if (opperand.substr(0, 2) == "#0") {
+  			opperand = "num";
+  		} else if (opperand.substr(0, 2) == "0x") {
+  			opperand = "mem";
+  		} else {
+  			opperand = "reg";
+  		}
+      //start concatenating to create objGram
+      objGram += "." + variable;
+      //start concatenating to create opcode
+  		opcode += "." + opperand;
+
+  	}
+    //assign opcode to specific object's objGram
+    objOpCode = opcode;
+
+  	return opcode;
+  }
 }
 
 /**
  * Loads all the grams we want to find
  */
-void loadGrams()
+/*void loadGrams()
 {
 	ifstream file("grams.txt");
 	string fileLine, sensor, gram;
@@ -67,23 +111,23 @@ void loadGrams()
 	// Loop though file
 	while (getline(file, fileLine)) {
 		istringstream stream(fileLine);
-		
+
 		getline(stream, sensor, ' ');
-		
+
 		// Loop to load grams
 		while (!stream.eof()) {
 			getline(stream, gram, ' ');
-			
+
 			grams[sensor].push_back(gram);
 		}
 	}
 
 	// Output grams
 	cout << "===== LOADED GRAMS =====" << endl;
-	
+
 	for (auto i = grams.begin(); i != grams.end(); i++) {
 		cout << i->first << endl;
-		
+
 		for (auto j = i->second.begin(); j != i->second.end(); j++) {
 			cout << "\t" << *j << endl;
 		}
@@ -96,13 +140,17 @@ void loadGrams()
 int main()
 {
 	loadGrams();
-	
+
 	ifstream sample("sample.txt");
 	string fileLine, instructionGram;
 
+  //list to keep track of values that have been seen
+  std::vector<string> usedValues;
+  int object_number = 0;
 	while (getline(sample, fileLine)) {
-		instructionGram = createGram(fileLine);
-		
+    instructionGram = instructionObj(fileLine, object_number);
+    object_number++;
+    std::cout << object_number << '\n';
 		//todo: check grams
 	}
 
