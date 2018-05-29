a2a74134beb850ce3cab33003dc17e887a327839
Ken Schimpf <kswissthefourth@Kens-MacBook-Pro.local>
2018-05-29 10:58:53 -0400

    tests and bug fixes

--- addr-finder.cpp
+++ addr-finder.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 // Features loaded in from config
-map<string, vector<string>> grams;
-
+/*map<string, vector<string>> grams;
+*/
 /**
  * Trim whitespace on string
  */
@@ -24,81 +24,82 @@ string trim(string& str)
 /**
  * Replaces opperands with normalized code
  */
- class instructionObj{
-   //class type
-   public:
+class instructionObj{
+  //class type
+  public:
 
-   //instance variables
-   std::vector<string> variables; //all variables in instruction line (ex: var1, var27, etc..)
-   std::vector<string> values; //all values in instruction line (ex: 0x102a, #0x80, al)
-   string objOpCode; //this has strings in it (ex: lda.num.mem)
-   string objGram;  //this has variables in it (ex: bcc.Var1.Var27)
-   int objNumber; //distinguishes each object from one another
+  //instance variables
+  std::vector<string> variables; //all variables in instruction line (ex: var1, var27, etc..)
+  std::vector<string> values; //all values in instruction line (ex: 0x102a, #0x80, al)
+  string objOpCode; //this has strings in it (ex: lda.num.mem)
+  string objGram;  //this has variables in it (ex: bcc.Var1.Var27)
+  int objNumber; //distinguishes each object from one another
+  bool noOpperands; //distinuguishes if a instruction line has opperands or not (ex: bcc[true] or lda al, 0x102f[false])
 
  //turn this into the object's constructor potentially
-  instructionObj(string& instruction, int& object_number){
-    //set object's objNumber
+  instructionObj(string& instruction, int& object_number,vector<string>& usedValues){
+  //set object's objNumber
     objNumber = object_number;
 
     //start filling object instances with file line data
     string opcode = instruction.substr(0, instruction.find(" "));
-
+    objGram = opcode;
   	string opperands = instruction.substr(opcode.size(), instruction.size());
-
+    string opperand;
   	// Check instructions with no opperands
   	if (opperands == "") {
-  		return opcode;
-  	}
+  		objOpCode = opcode;
+      noOpperands = true; //this instruction does not have opperands
+  	}else{
 
-  	stringstream stream(opperands);
-  	string opperand;
+      stringstream stream(opperands);
 
-  	// Loop through all instruction opperands
-  	while (stream.good()) {
-  		getline(stream, opperand, ',');
-  		opperand = trim(opperand);
+      // Loop through all instruction opperands
+      while (stream.good()) {
+        getline(stream, opperand, ',');
+        opperand = trim(opperand);
 
-      //add operand to values array for current object (Needs updating)
-      values.push_back(opperand);
+        string variable; //instantiate variable
+        //add operand to values array for current object (Needs updating)
+        values.push_back(opperand);
 
-      //USED FOR CREATING objGram
-      //loop through usedValues vector to see if the opperand has been used
-      int position = find(usedValues.begin(), usedValues.end(), opperand) - usedValues.begin())
-      if (position < usedValues.size()){
-        //variable has already been used
-        //create a variable based on that index and add to current object's variables array
-        string variable = "Var" + to_string(position);
-        variables.push_back(variable);
-      }else{
-        //add opperand to end of the usedValues vector
-        usedValues.push_back(opperand);
-        //get index where operand was just placed in array and create variable based on it
-        string variable = "Var" + to_string(usedValues.size() - 1);
-        //add new variable to instruction object's variable vector (needs updating)
-        variables.push_back(variable);
+        //USED FOR CREATING objGram
+        //loop through usedValues vector to see if the opperand has been used
+        int position = find(usedValues.begin(), usedValues.end(), opperand) - usedValues.begin();
+        if (position < usedValues.size()){
+          //variable has already been used
+          //create a variable based on that index and add to current object's variables array
+          variable = "Var" + to_string(position);
+          variables.push_back(variable);
+        }else{
+          //add opperand to end of the usedValues vector
+          usedValues.push_back(opperand);
+          //get index where operand was just placed in array and create variable based on it
+          variable = "Var" + to_string(usedValues.size() - 1);
+          //add new variable to instruction object's variable vector (needs updating)
+          variables.push_back(variable);
+        }
 
+        //USED FOR CONSTRUCTING objOpCode
+        if (opperand.substr(0, 2) == "#0") {
+          opperand = "num";
+        } else if (opperand.substr(0, 2) == "0x") {
+          opperand = "mem";
+        } else {
+          opperand = "reg";
+        }
+        //start concatenating to create objGram
+        objGram += "." + variable;
+        //start concatenating to create opcode
+        opcode += "." + opperand;
+
+        //assign opcode to specific object's objGram
       }
-
-      //USED FOR CONSTRUCTING objOpCode
-  		if (opperand.substr(0, 2) == "#0") {
-  			opperand = "num";
-  		} else if (opperand.substr(0, 2) == "0x") {
-  			opperand = "mem";
-  		} else {
-  			opperand = "reg";
-  		}
-      //start concatenating to create objGram
-      objGram += "." + variable;
-      //start concatenating to create opcode
-  		opcode += "." + opperand;
-
-  	}
-    //assign opcode to specific object's objGram
-    objOpCode = opcode;
-
-  	return opcode;
+      objOpCode = opcode;
+      noOpperands = false; //this instruction has opperands
+    }
   }
-}
+};
 
 /**
  * Loads all the grams we want to find
@@ -134,24 +135,29 @@ string trim(string& str)
 	}
 }
 
-/**
+
  * Start of program
  */
 int main()
 {
-	loadGrams();
+	//loadGrams();
 
 	ifstream sample("sample.txt");
 	string fileLine, instructionGram;
 
   //list to keep track of values that have been seen
   std::vector<string> usedValues;
+  std::vector<instructionObj> objects;
   int object_number = 0;
 	while (getline(sample, fileLine)) {
-    instructionGram = instructionObj(fileLine, object_number);
+    instructionObj foo(fileLine, object_number, usedValues);
+    objects.push_back(foo);
+    //std::cout << object_number << '\n';
+	  //std::cout << usedValues.front() << '\n';
+    std::cout << foo.objGram<< '\n';
+    std::cout << foo.objOpCode<< '\n';
+    std::cout << objects.size()<< '\n';
     object_number++;
-    std::cout << object_number << '\n';
-		//todo: check grams
 	}
 
 	// Cleanup
